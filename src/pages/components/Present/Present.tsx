424384294e75dd3670416f57a99606a7bde86508
Debora Borges <debora@lauduz.com>
2025-02-03 14:54:39 -0300

    criação do componente de laço

---
+++ src/pages/components/Present/Present.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles.module.css"
+
+
+export default function Present() {
+    return (
+    <div className={styles.present}>
+         <div className={styles.lid}></div>
+         <div className={styles.boxBody}></div>
+         <div className={styles.laco1}></div>
+         <div className={styles.laco2}></div>
+    </div>
+    );
+  }
+  
