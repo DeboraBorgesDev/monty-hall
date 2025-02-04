f918a0d2b8ea5bd4ad1f6f7d46141a7d221a82bb
Debora Borges <debora@lauduz.com>
2025-02-04 08:37:01 -0300

    estilo da porta selecionada

--- src/pages/components/Door/index.tsx
+++ src/pages/components/Door/index.tsx
@@ -1,9 +1,14 @@
 import styles from "./styles.module.css"
 
-export default function Door(porps: any) {
+type DoorProps = {
+    selected?: boolean
+}
+
+export default function Door(props: DoorProps) {
+    const selected = props.selected ? styles.selected : ''
     return (
        <div className={styles.area}>
-         <div className={styles.frame}>
+         <div className={`${styles.frame} ${selected}`}>
             <div className={styles.door}>
                 <div className={styles.number}>3</div>
                 <div className={styles.knob}></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div>
-       <Door/>
+       <Door selected/>
       </div>
     </>
   );
